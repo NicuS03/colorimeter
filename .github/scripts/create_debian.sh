f9b636058715bfafa75f3de9280d246197986939
Nicu Siderias <nicu.siderias@analog.com>
2025-09-02 15:58:34 +0300

    Update verify_install.sh

--- .github/scripts/create_debian.sh
+++ .github/scripts/create_debian.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 
-tar_name=$1
-version=$2
-architecture=$3
+version=$1
+architecture=$2
 
 source_code=$(basename "$PWD")
 
-chmod +x CI/install_libiio.sh && ./CI/install_libiio.sh
-sudo mkdir /usr/share/desktop-directories/
 sudo apt update
-sudo apt install -y build-essential make devscripts debhelper python3-gi-cairo
+sudo apt install -y build-essential make devscripts debhelper pybuild-plugin-pyproject python3 python3-gi-cairo libaio1 policykit-1 libserialport0
 
 #Replace placeholders inside the debian template files
-sed -i "s/@VERSION@/$version/" packaging/debian/changelog
+sed -i "s/@VERSION@/$version-1/" packaging/debian/changelog
 sed -i "s/@DATE@/$(date -R)/" packaging/debian/changelog
 sed -i "s/@ARCHITECTURE@/$architecture/" packaging/debian/control
 
@@ -20,7 +17,11 @@ cp -r packaging/debian .
 chmod +x debian/rules
 
 pushd ..
-tar czf $tar_name $source_code
-
+tar czf $source_code\_$version.orig.tar.gz $source_code
+wget -qO- "https://api.github.com/repos/analogdevicesinc/libiio/releases/latest"   | grep "browser_download_url" | grep -Eo '"https.*(Ubuntu-22|arm).*deb"$' | xargs -n 1 wget -q
+for file in *.deb; do
+    dpkg -I $file | grep -q "Architecture: $architecture" && echo "Installing libiio $file" && sudo dpkg -i $file && break
+done
 popd
+
 debuild
--- .github/scripts/verify_install.sh
+++ .github/scripts/verify_install.sh
@@ -11,12 +11,12 @@ if [ ! -d "$install_location/share/adi_colorimeter/" ] || [ ! -d "$install_locat
 fi
 
 if [ ! -f "$install_location/bin/adi_colorimeter" ] || [ ! -f "$install_location/lib/adi_colorimeter/capture.so" ] || \
-    [ ! -f "$install_location/share/applications/adi_colorimeter.glade" ] || \
-    [ ! -f "$install_location/share/polkit-1/actions/com.analogdevicesinc.adi_colorimeter.policy" ] || \
-    [ ! -f "$install_location/share/applications/adi_colorimeter.desktop" ] || \
-    [ ! -f "$install_location/share/icons/hicolor/16x16/apps/adi_colorimeter.png" ] || \
-    [ ! -f "$install_location/share/icons/hicolor/32x32/apps/adi_colorimeter.png" ] || \
-    [ ! -f "$install_location/share/icons/hicolor/64x64/apps/adi_colorimeter.png" ]; then
+    [ ! -f "$install_location/share/adi_colorimeter/adi_colorimeter.glade" ] || \
+    [ ! -f "$install_location/share/polkit-1/actions/org.adi.pkexec.adi_colorimeter.policy" ] || \
+    [ ! -f "$install_location/share/applications/adi-colorimeter.desktop" ] || \
+    [ ! -f "$install_location/share/icons/hicolor/16x16/apps/adi-colorimeter.png" ] || \
+    [ ! -f "$install_location/share/icons/hicolor/32x32/apps/adi-colorimeter.png" ] || \
+    [ ! -f "$install_location/share/icons/hicolor/64x64/apps/adi-colorimeter.png" ]; then
         echo "Installation files not found!"
         exit 1
 fi
